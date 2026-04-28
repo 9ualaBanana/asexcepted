f1dfb059736d05148090dc0333e4e2532507ddc6
gualabanana <9ualabanana@gmail.com>
2026-04-28 10:05:06 +0700

    Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { PwaInit } from "@/components/pwa/pwa-init";
 import "./globals.css";
 
@@ -48,6 +49,7 @@ export default function RootLayout({
           <PwaInit />
           {children}
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
